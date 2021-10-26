ddcfb33c8193b6fd9eaaa90467c1d2c53a4c07b2
Tony Arteaga <51865661+Memo2704@users.noreply.github.com>
2021-10-25 21:54:06 -0300

    Added spring initializer.

---
+++ src/main/java/com/tonyarteaga/rest/webservices/restfulrestservices/RestfulRestServicesApplication.java
@@ -0,0 +1,13 @@
+package com.tonyarteaga.rest.webservices.restfulrestservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulRestServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulRestServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tonyarteaga/rest/webservices/restfulrestservices/RestfulRestServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonyarteaga.rest.webservices.restfulrestservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulRestServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
