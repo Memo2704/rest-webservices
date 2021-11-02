af17fdeaf11d1d9e25c69fb6610905e1071840ee
Tony Arteaga <51865661+Memo2704@users.noreply.github.com>
2021-11-02 16:35:13 -0300

    Added HATEOAS and validation of resources

--- src/main/java/com/tonyarteaga/rest/webservices/restfulrestservices/RestfulRestServicesApplication.java
+++ src/main/java/com/tonyarteaga/rest/webservices/restfulrestservices/RestfulRestServicesApplication.java
@@ -1,13 +1,13 @@
 package com.tonyarteaga.rest.webservices.restfulrestservices;
 
 import com.tonyarteaga.rest.webservices.restfulrestservices.helloworld.HelloWorldController;
+import com.tonyarteaga.rest.webservices.restfulrestservices.user.UserResource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"com.tonyarteaga.rest.webservices.restfulrestservices.user.UserResource.class",
-		"com.tonyarteaga.rest.webservices.restfulrestservices.exception.CustomizedResponseEntityExceptionHandler"})
+@ComponentScan(basePackageClasses = UserResource.class)
 public class RestfulRestServicesApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/tonyarteaga/rest/webservices/restfulrestservices/user/UserResource.java
+++ src/main/java/com/tonyarteaga/rest/webservices/restfulrestservices/user/UserResource.java
@@ -1,12 +1,19 @@
 package com.tonyarteaga.rest.webservices.restfulrestservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserResource {
@@ -20,17 +27,21 @@ public class UserResource {
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id) {
+    public EntityModel<User> retrieveUser(@PathVariable int id) {
         User user = service.findOne(id);
         if (user == null){
             throw new UserNotFoundException("id-" + id);
         }
-        return user;
+
+        EntityModel<User> model = EntityModel.of(user);
+        WebMvcLinkBuilder linkToUsers = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        model.add(linkToUsers.withRel("all-users"));
+        return model;
 
     }
 
     @PostMapping("/users")
-    public ResponseEntity<Object> createUser(@RequestBody User user) {
+    public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
         User savedUser = service.save(user);
 
         URI location = ServletUriComponentsBuilder.
