55fed270fa8a6ea4c947a60b97baf1e9afa709ac
Tony Arteaga <51865661+Memo2704@users.noreply.github.com>
2021-11-04 19:23:31 -0300

    Added Spring HAL and Spring Actuator

--- src/main/java/com/tonyarteaga/rest/webservices/restfulrestservices/helloworld/HelloWorldController.java
+++ src/main/java/com/tonyarteaga/rest/webservices/restfulrestservices/helloworld/HelloWorldController.java
@@ -1,11 +1,18 @@
 package com.tonyarteaga.rest.webservices.restfulrestservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 // Controller
 @RestController
 public class HelloWorldController {
 
+    @Autowired
+    private MessageSource messageSource;
     // GET
     // URI /hello-world
     //method - "Hello World"
@@ -20,5 +27,13 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(
+            @RequestHeader(name="Accept-Language", required=false) Locale locale
+    ){
+        return messageSource.getMessage("good.morning.message", null, "Default Message", LocaleContextHolder.getLocale());
+        //return "Hello World";
     }
 }
